2f8403e0a22ab36fabd6e118a387d5cf1b28bf36
Álvaro Bartolomé del Canto <alvarob96@usal.es>
2019-08-12 09:51:53 +0200

    v0.0.8 setup
    - so to fix version error between github and pypi

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ def readme():
 
 setup(
     name='twipper',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     url='https://github.com/alvarob96/twipper',
-    download_url='https://github.com/alvarob96/twipper/archive/0.0.7.tar.gz',
+    download_url='https://github.com/alvarob96/twipper/archive/0.0.8.tar.gz',
     license='GNU General Public License v3 (GPLv3)',
     author='Alvaro Bartolome',
     author_email='alvarob96@usal.es',
